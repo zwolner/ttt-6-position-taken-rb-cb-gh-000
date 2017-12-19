11de55b02b5a12e5a4835fd74c5404c6c1358451
Zachary Wolner <zachary.wolner@gmail.com>
2017-12-19 20:33:26 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == "X" || board[index] == "O"
     true
-  elsif board[index] == "" || board[index] == " " || board[index] == nil
+  elsif board[index] == ("" || " " || nil)
     false
   end
 end
